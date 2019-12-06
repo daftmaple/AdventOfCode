3c83563494aece0a08821ebe0bf342f9275c9605
daftmaple <ubuntumaple@gmail.com>
2019-12-06 23:56:40 +1100

    Day 6

---
+++ 2019/q06a.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+open F, "<q06_input.txt" or die;
+
+my %dictionary;
+my %node;
+my $total = 0;
+
+while (<F>) {
+    chomp $_;
+    my @split = split /\)/, $_;
+    $dictionary{$split[1]} = $split[0];
+    $node{$split[1]} += 1;
+}
+
+
+for (keys %node) {
+    # $_ is a distinct value of child (or parent, but will never be root node)
+    # Skip if not one, since it is not the last child
+    next if $node{$_} != 1;
+    $e = $_;
+    while (exists $dictionary{$e}) {
+        $e = $dictionary{$e};
+        $total++;
+    }
+}
+
+print "$total\n";
---
+++ 2019/q06b.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+
+open F, "<q06_input.txt" or die;
+
+my %dictionary;
+my $total = -2;
+my $common = '';
+
+while (<F>) {
+    chomp $_;
+    my @split = split /\)/, $_;
+    $dictionary{$split[1]} = $split[0];
+}
+
+my @san;
+my @you;
+
+$e = $dictionary{'SAN'};
+while (exists $dictionary{$e}) {
+    push @san, $e;
+    $e = $dictionary{$e};
+}
+
+$f = $dictionary{'YOU'};
+while (exists $dictionary{$f}) {
+    push @you, $f;
+    $f = $dictionary{$f};
+}
+
+my $i = 0;
+foreach my $y (@you) {
+    $i++;
+    my $j = 0;
+    foreach my $s (@san) {
+        $j++;
+        if ($y eq $s) {
+            $total += ($i + $j);
+        }
+    }
+    last if ($total >= 0);
+}
+
+print "$total\n";
