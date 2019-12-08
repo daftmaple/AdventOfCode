9165b0ccb3eb110118b297c0f076921892c2bf09
daftmaple <ubuntumaple@gmail.com>
2019-12-08 16:52:41 +1100

    Day 8

---
+++ 2019/q08a.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+open F, "<q08_input.txt" or die;
+$str = <F>;
+close F;
+
+@arr = ();
+
+$layer = 25 * 6;
+
+$i = 0;
+while ($i < length($str) / $layer) {
+    $input = substr($str, $i * $layer, $layer);
+    push @arr, $input;
+    $i++;
+}
+
+$zeros = 100;
+$leastidx = 0;
+$i = 0;
+foreach $k (@arr) {
+    $count = count($k, 0);
+    # print "$i, $count: $k\n";
+    if ($count < $zeros) {
+        $leastidx = $i;
+        $zeros = $count;
+    }
+    $i++;
+}
+
+print count($arr[$leastidx], 1) * count($arr[$leastidx], 2);
+print "\n";
+
+sub count {
+    $chk = shift @_;
+    $no = shift @_;
+    # print "$chk\n";
+    @chopped = split //, $chk;
+    $ct = 0;
+    foreach $char (@chopped) {
+        # print "$char $ct\n";
+        $ct++ if ($char == $no);
+    }
+    return $ct;
+}
+
---
+++ 2019/q08b.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+open F, "<q08_input.txt" or die;
+$str = <F>;
+close F;
+
+@arr = ();
+
+$layer = 25 * 6;
+
+$i = 0;
+while ($i < length($str) / $layer) {
+    $input = substr($str, $i * $layer, $layer);
+    push @arr, $input;
+    $i++;
+}
+
+$zeros = 100;
+$leastidx = 0;
+$i = 0;
+
+@overall = (2) x $layer;
+
+foreach $k (@arr) {
+    @chopped = split //, $k;
+    $j = 0;
+    while ($j < (@chopped)) {
+        $overall[$j] = $chopped[$j] if $overall[$j] == 2;
+        $j++;
+    }
+    $i++;
+}
+
+$i = 0;
+while ($i < (@overall)) {
+    if ($overall[$i] == 1) {
+        print '#';
+    } else {
+        print ' ';
+    }
+    $i++;
+    print "\n" if $i % 25 == 0; 
+}
