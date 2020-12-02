e45981ccff0b01942ac6c4b5a427423cd4037632
daftmaple <ubuntumaple@gmail.com>
2020-12-02 19:15:40 +1100

    Day 8 of 2015

---
+++ 2015/q08/q08a.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+
+my @strings = ();
+open F, "<q08_input.txt" or die;
+while (<F>) {
+    chomp;
+    push @strings, $_;
+}
+close F;
+
+my $literal = 0;
+my $minus = 0;
+my $hex = 0;
+while (@strings) {
+    my $word = shift (@strings);
+    $literal += length($word);
+    
+    # Remove enclosing quotes
+    my $simplifiedWord = ($word =~ /^\"(.*)\"$/)[0];
+
+    # Capture all escaped
+    my @escaped = ($simplifiedWord =~ /(\\)[\\"]/g);
+
+    # Capture all hexes
+    my @hexes = ($simplifiedWord =~ /(\\x[0-9a-f]{2})/g);
+
+    # Length must be length of word minus:
+    # - 2 from quotes
+    # - size of all escaped
+    # - 3 * size of all hexes
+    my $mem = length($word) - (2 + scalar(@escaped) + 3 * scalar(@hexes));
+    $minus += $mem;
+}
+
+my $result = $literal - $minus;
+
+print "$result\n";
---
+++ 2015/q08/q08b.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+
+my @strings = ();
+open F, "<q08_input.txt" or die;
+while (<F>) {
+    chomp;
+    push @strings, $_;
+}
+close F;
+
+my $literal = 0;
+my $plus = 0;
+my $hex = 0;
+while (@strings) {
+    my $word = shift (@strings);
+    $literal += length($word);
+    
+    # Remove enclosing quotes
+    my $simplifiedWord = ($word =~ /^\"(.*)\"$/)[0];
+
+    # Capture all escaped
+    my @escaped = ($simplifiedWord =~ /(\\)[\\"]/g);
+
+    # Capture all hexes
+    my @hexes = ($simplifiedWord =~ /(\\x[0-9a-f]{2})/g);
+
+    # Length of the new one:
+    # + 4 from quotes
+    # + 2 * size of all escaped
+    # + size of all hexes
+    my $mem = length($word) + (4 + 2 * scalar(@escaped) + scalar(@hexes));
+    $plus += $mem;
+}
+
+my $result = $plus - $literal;
+
+print "$result\n";
