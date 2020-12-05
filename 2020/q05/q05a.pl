e2dfb67f83f2e0cb9f63651fbc956c075c342851
daftmaple <ubuntumaple@gmail.com>
2020-12-05 19:05:42 +1100

    Optimize binary to dec

--- 2020/q05/q05a.pl
+++ 2020/q05/q05a.pl
@@ -7,19 +7,11 @@ while (<F>) {
     s/[FL]/0/g;
     s/[BR]/1/g;
     my $res = $_;
-    my $bin1 = substr $res, 0, 7;
-    my $row = bin2dec($bin1);
-
-    my $bin2 = substr $res, 7, 10;
-    my $col = bin2dec($bin2);
-
-    my $sID = $row * 8 + $col;
+    my $row = substr $res, 0, 7;
+    my $col = substr $res, 7, 10;
+    my $sID = oct("0b$row") * 8 + oct("0b$col");
 
     $highest = $sID if $sID > $highest;
 }
 
 print "$highest\n";
-
-sub bin2dec {
-    return unpack("N", pack("B32", substr("0" x 32 . shift, -32)));
-}
--- 2020/q05/q05b.pl
+++ 2020/q05/q05b.pl
@@ -7,13 +7,9 @@ while (<F>) {
     s/[FL]/0/g;
     s/[BR]/1/g;
     my $res = $_;
-    my $bin1 = substr $res, 0, 7;
-    my $row = bin2dec($bin1);
-
-    my $bin2 = substr $res, 7, 10;
-    my $col = bin2dec($bin2);
-
-    my $sID = $row * 8 + $col;
+    my $row = substr $res, 0, 7;
+    my $col = substr $res, 7, 10;
+    my $sID = oct("0b$row") * 8 + oct("0b$col");
 
     push @array, $sID;
 }
@@ -26,9 +22,4 @@ for (my $i = 0; $i < scalar(@sorted) - 1; $i++) {
         print "$result\n";
         exit;
     }
-}
-
-
-sub bin2dec {
-    return unpack("N", pack("B32", substr("0" x 32 . shift, -32)));
 }
