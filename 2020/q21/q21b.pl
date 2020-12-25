5c3be1c8879f34de83aa7629231104e218a8ce00
daftmaple <ubuntumaple@gmail.com>
2020-12-25 17:22:09 +1100

    Day 21 part 2 of 2020

--- 2020/q21/q21b.pl
+++ 2020/q21/q21b.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 
+use List::Util qw(reduce);
+
 open F, "q21_input.txt" or die;
 chomp(my @input = <F>);
 close F;
@@ -37,20 +39,39 @@ foreach my $line (@input) {
     }
 }
 
-# Flatten the hash
-my %possible;
-foreach my $allergen (keys %hash) {
-    my @i = @{$hash{$allergen}};
-    $possible{$_}++ foreach @i;
-}
+# Find the corresponding ingredient for the allergen
+# Map of allergen -> ingredient
+my %map;
+my %mappedIngredient;
 
-my $result = 0;
-foreach my $line (@input) {
-    my ($i) = ($line =~ /^(.*) \(contains .*\)$/g);
-    my @ingredients = ($i =~ /(\w+)+/g);
-    foreach my $item (@ingredients) {
-        $result++ if !defined($possible{$item});
+while (scalar(keys %map) < scalar(keys %hash)) {
+    # Loop through list of ingredients until all ingredients are found
+    foreach my $allergen (keys %hash) {
+        # Allergen that is already mapped can be skipped
+        next if defined($map{$allergen});
+        my @i = @{$hash{$allergen}};
+        if (scalar(@i) == 1) {
+            # Item in this array corresponds to the allergen
+            my $ingredient = shift @i;
+            $map{$allergen} = $ingredient;
+            $hash{$allergen} = \@i;
+            $mappedIngredient{$ingredient}++;
+        } else {
+            # Loop through @i, remove any item that is already in %mappedIngredient
+            # Use a new array
+            my @newI = ();
+            foreach my $item (@i) {
+                unshift @newI, $item if !defined $mappedIngredient{$item};
+            }
+            $hash{$allergen} = \@newI;
+        }
     }
 }
+
+my @allergens = sort keys %map;
+my @ingredients = ();
+push @ingredients, $map{$_} foreach @allergens;
+
+my $result = reduce {"$a,$b"} @ingredients;
 
 print "$result\n";
